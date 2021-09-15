d3c4b3854848c90a87fef0fe42c1e8bc72896a85
Kowsiya Srikantharajah <srikanko@sheridancollege.ca>
2021-09-15 19:12:28 -0400

    Removes image -min tags

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -111,7 +111,7 @@ fetch('http://localhost:8080/mamasdish/countryRecipes')
 					}
 					if (x < MAX_SIZE){
 						contentString += "<li style='border:0px; padding:0px; margin-left:0px; width:400px;'>"
-						contentString += "<div class='row' style='border: 2px solid #e4e8eb; padding:10px; border-radius: 15px;'><img class='col s4' style='height:70px; margin-left:0px;' src='/images/recipes/" + recipeImg + "'>"
+						contentString += "<div class='row' style='border: 2px solid #e4e8eb; padding:10px; border-radius: 15px;'><img class='col s4' style='height:70px; margin-left:0px;' src='/images/recipes/" + recipeImg + ".jpeg'>"
 						contentString += "<a id='discoverFont' class='col s8' style='margin-top:10px; padding-top:0px;' href=/users/viewRecipe/"+ data[i].recipes[x].recipeId + ">"
 						contentString += data[i].recipes[x].recipeTitle + "</a></div></li>"						
 					}
