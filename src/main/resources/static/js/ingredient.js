6294f898f5e739811783486a144619bb6de34378
cocobilaal <rashidb@sheridancollege.ca>
2021-09-30 13:12:48 -0400

    Added variables for nutrition

--- src/main/resources/static/js/ingredient.js
+++ src/main/resources/static/js/ingredient.js
@@ -7,32 +7,32 @@ function saveRecipe() {
 
 	for (let ingrName of ingredientNames) {
 
-			if (ingrName.value == "") {
-	
-				ingredientCheck = false;
-				$("#errId").show();
-				
-				break;
-				
-			}
+		if (ingrName.value == "") {
+
+			ingredientCheck = false;
+			$("#errId").show();
+
+			break;
+
+		}
 
 	}
 
-	if (ingredientCheck) {		
-		
+	if (ingredientCheck) {
+
 		//add variables 
 
-		for(let ingrDivs of $('[id^="ingredientDiv"]').children()){
+		for (let ingrDivs of $('[id^="ingredientDiv"]').children()) {
 
 
-			var ingredient = $("#"+ingrDivs.id).find('input[id^="ingredientName"]').get(0).value;
-			var measurement = $("#"+ingrDivs.id).find('select[id^="measurement"]').get(0).value;
-			var quantity = $("#"+ingrDivs.id).find('input[id^="quantity"]').get(0).value;
+			var ingredient = $("#" + ingrDivs.id).find('input[id^="ingredientName"]').get(0).value;
+			var measurement = $("#" + ingrDivs.id).find('select[id^="measurement"]').get(0).value;
+			var quantity = $("#" + ingrDivs.id).find('input[id^="quantity"]').get(0).value;
 			//console.log($("#"+ingrDivs.id).find('input[id^="quantity"]').get());
-			console.log($("#"+ingrDivs.id).find('select[id^="proteinType"]').get());
-			var protein = $("#"+ingrDivs.id).find('select[id^="proteinType"]').get(0).value;
-			
-			var el = $("#"+ingrDivs.id).find('input[id^="protein"]').get(0);
+			console.log($("#" + ingrDivs.id).find('select[id^="proteinType"]').get());
+			var protein = $("#" + ingrDivs.id).find('select[id^="proteinType"]').get(0).value;
+
+			var el = $("#" + ingrDivs.id).find('input[id^="protein"]').get(0);
 			if (el.checked) {
 				console.log(protein)
 				if (protein == null) {
@@ -63,6 +63,15 @@ function saveRecipe() {
 			console.log("measurement: " + measurement);
 			console.log("quantity:" + quantity);
 			console.log("protein:" + protein);
+
+			// Nutrition API Call
+			curl - XPOST https://api.nutritionix.com/v1_1/search -H 'Content-Type: application/json' -d'
+			{
+				"appId": "52c550ac",
+				"appKey": "c9873f02bd95c74d5de0934edd09ff7a",
+				"query": quantity + " " + measurement + " " + ingredient
+			}	
+			
 
 
 			fetch('http://localhost:8080/mamasdish/addIngredient/' + ingredient + '/' + quantity + '/' + measurement + '/' + recipeId + '/' + protein)
@@ -89,36 +98,36 @@ function editRecipe() {
 
 	for (let ingrName of ingredientNames) {
 
-			if (ingrName.value == "") {
-	
-				ingredientCheck = false;
-				$("#errId").show();
-				
-				break;
-				
-			}
+		if (ingrName.value == "") {
+
+			ingredientCheck = false;
+			$("#errId").show();
+
+			break;
+
+		}
 
 	}
 
-	if (ingredientCheck) {		
+	if (ingredientCheck) {
 
-		fetch('http://localhost:8080/mamasdish/deleteIngredients/' + recipeId )
+		fetch('http://localhost:8080/mamasdish/deleteIngredients/' + recipeId)
 			.then(data => data.json())
 			.then(function(data) {
-			console.log(data);
-		});
-		
-		for(let ingrDivs of $('[id^="ingredientDiv"]').children()){
+				console.log(data);
+			});
+
+		for (let ingrDivs of $('[id^="ingredientDiv"]').children()) {
 
 
-			var ingredient = $("#"+ingrDivs.id).find('input[id^="ingredientName"]').get(0).value;
-			var measurement = $("#"+ingrDivs.id).find('select[id^="measurement"]').get(0).value;
-			var quantity = $("#"+ingrDivs.id).find('input[id^="quantity"]').get(0).value;
+			var ingredient = $("#" + ingrDivs.id).find('input[id^="ingredientName"]').get(0).value;
+			var measurement = $("#" + ingrDivs.id).find('select[id^="measurement"]').get(0).value;
+			var quantity = $("#" + ingrDivs.id).find('input[id^="quantity"]').get(0).value;
 			//console.log($("#"+ingrDivs.id).find('input[id^="quantity"]').get());
-			console.log($("#"+ingrDivs.id).find('select[id^="proteinType"]').get());
-			var protein = $("#"+ingrDivs.id).find('select[id^="proteinType"]').get(0).value;
-			
-			var el = $("#"+ingrDivs.id).find('input[id^="protein"]').get(0);
+			console.log($("#" + ingrDivs.id).find('select[id^="proteinType"]').get());
+			var protein = $("#" + ingrDivs.id).find('select[id^="proteinType"]').get(0).value;
+
+			var el = $("#" + ingrDivs.id).find('input[id^="protein"]').get(0);
 			if (el.checked) {
 				console.log(protein)
 				if (protein == null) {
@@ -161,7 +170,7 @@ function editRecipe() {
 		}
 
 		//go to instruction page
-		window.open('/chefs/editInstructions/' +recipeId, '_self');
+		window.open('/chefs/editInstructions/' + recipeId, '_self');
 
 	}
 }
@@ -174,47 +183,47 @@ function showProtein(el) {
 	if (el.checked) {
 		$("#proteinDiv" + divId[1]).css("display", "block")
 	}
-	else{
+	else {
 		$("#proteinDiv" + divId[1]).css("display", "none")
 	}
 }
 
 
 
-function getNewId(){
+function getNewId() {
 
-	
+
 	var newDivId = true;
-	
-	var divID = Math.floor(Math.random() * 100);
-	
 
-	while(checkId(divID) == false){
-	
-			divID = Math.floor(Math.random() * 100);
+	var divID = Math.floor(Math.random() * 100);
+
+
+	while (checkId(divID) == false) {
+
+		divID = Math.floor(Math.random() * 100);
 	}
-	
+
 	return divID;
-	
+
 }
 
 
-function checkId(id){
+function checkId(id) {
 
-	for(let ingrDivs of $('[id^="ingredientBox"]')){
-			var num = (ingrDivs.id).split("ingredientBox");
-			var num = num[1];
-			
-			
-			if(num == id){
-				return false;
-			
-			} else {
-			
-				return true;
-			}
-			
+	for (let ingrDivs of $('[id^="ingredientBox"]')) {
+		var num = (ingrDivs.id).split("ingredientBox");
+		var num = num[1];
+
+
+		if (num == id) {
+			return false;
+
+		} else {
+
+			return true;
 		}
+
+	}
 
 }
 
@@ -222,9 +231,9 @@ function checkId(id){
 function newIngredient() {
 
 	var measurements = getMeasurements();
-	
+
 	var divID = getNewId();
-	
+
 
 	console.log(divID);
 	var proteinTypes = getProteins();
@@ -246,8 +255,8 @@ function newIngredient() {
 		"<option value=''  selected disabled></option>" +
 		"</select> </div> </div> </div> </div> " +
 		"<div class='col s6' style='padding-top:90px'>" +
-			"<a class='btn-floating' onclick='deleteIngredient("+divID+")' id='deleteBtn'>"+
-				"<i class='small material-icons'>delete_forever</i>" +
+		"<a class='btn-floating' onclick='deleteIngredient(" + divID + ")' id='deleteBtn'>" +
+		"<i class='small material-icons'>delete_forever</i>" +
 		"</a></div></div></div>");
 
 
@@ -271,14 +280,14 @@ function newIngredient() {
 
 }
 
-function deleteIngredient(id){
-		
-	
-	if(($('[id^="ingredientDiv"]').children()).length >1){
-	
-		$("#ingredientBox"+id).remove();
-	
+function deleteIngredient(id) {
+
+
+	if (($('[id^="ingredientDiv"]').children()).length > 1) {
+
+		$("#ingredientBox" + id).remove();
+
 	}
 
-	
+
 }
